15ca32db79cd8158ea33d3cd853156ff54928a4c
Reny Reyes <renyreyes8@hotmail.com>
2022-11-15 14:59:57 -0500

    Added the csv for the index cards + Completed the flip_card function

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from tkinter import *
 from tkinter import ttk
-RED = "#92140C"
-GREEN = "#003E1F"
-BACKGROUND = "#808080"
+import pandas
+import random
+
+# COLORS USED
+BACKGROUND = "#6C7D47"
+NAVY = "#324A5F"
+
+# FUNCTIONALITY
+data = pandas.read_csv("data/french_words.csv")
+cards = data.to_dict(orient="records")
+
+def next_card():
+    current_card = random.choice(cards)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+    return current_card
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill=NAVY)
+    canvas.itemconfig(card_word, text= value["English"], fill=NAVY)
+
 
 # WINDOW SETTINGS
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=60, pady=60, bg=BACKGROUND)
 
-
 # ANSWER SIDE OF THE INDEX CARD
 canvas = Canvas(width=768, height=450)
-front_card_img = PhotoImage(file="images/back_card_img.png")
-canvas.create_image(384, 230, image=front_card_img)
-canvas.create_text(384, 230, text="Answer", font=("Arial", 40, "italic"))
+card_image = PhotoImage(file="images/card.png")
+canvas_image = canvas.create_image(384, 230, image=card_image)
+card_title = canvas.create_text(384, 115, text="", font=("Arial", 20, "italic"))
+card_word = canvas.create_text(384, 230, text="", font=("Arial", 40, "bold"))
 canvas.grid(row=0, column=0, columnspan=3)
 
 separator = ttk.Separator(window, orient='horizontal')
@@ -22,17 +40,19 @@ separator = ttk.Separator(window, orient='horizontal')
 
 # WRONG ANSWER BUTTON
 cross_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=cross_image)
+wrong_button = Button(image=cross_image, borderwidth=0, bg=BACKGROUND, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 # RIGHT ANSWER BUTTON
 check_image = PhotoImage(file="images/right.png")
-right_button = Button(image=check_image)
+right_button = Button(image=check_image, borderwidth=0, bg=BACKGROUND, command=next_card)
 right_button.grid(row=1, column=2)
 
 # FLIP THE CARD BUTTON
 flip_image = PhotoImage(file="images/flip.png")
-flip_button = Button(image=flip_image)
+flip_button = Button(image=flip_image, borderwidth=0, bg=BACKGROUND, command=flip_card)
 flip_button.grid(row=1,column=1)
 
+# RUN APP
+value = next_card()
 window.mainloop()
